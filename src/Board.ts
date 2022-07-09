3eed65da8d5450bf313f153b8ed52cbb087a6473
yankuchinsky <89603879035@yandex.ru>
2022-07-09 17:01:47 +0200

    Remove unused function

--- src/Board.ts
+++ src/Board.ts
@@ -204,22 +204,6 @@ class Board {
     }
   }
 
-  isVacantCell = (curr: number, prev: number) => {
-    const boardMap = this.getFlatBoard()
-    if (!boardMap[curr].piece) {
-      return true;
-    }
-
-    const currentColor = boardMap[prev].piece?.toString().split("")[0];
-    const movePieceColor = boardMap[curr].piece?.toString().split("")[0];
-   
-    if (currentColor !== movePieceColor) {
-      return true;
-    }
-  
-    return false;
-  };
-
   getCoordinates(position) {
     for (const fileId in this.newBoardMap) {
       for (const posId in this.newBoardMap[fileId]) {
