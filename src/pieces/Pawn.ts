56de0a77e7ead6c83df5c9e1e103f3d4111ccb4b
yankuchinsky <89603879035@yandex.ru>
2022-07-15 00:12:32 +0200

    update pawn capture

--- src/pieces/Pawn.ts
+++ src/pieces/Pawn.ts
@@ -1,5 +1,5 @@
 import Piece from './Piece';
-import { upMove } from '../helpers';
+import { getCoordinatesByPosition, getPositionByCoordinates, Position, upMove } from '../helpers';
 
 class Pawn extends Piece {
   private isMoved = false;
@@ -13,6 +13,31 @@ class Pawn extends Piece {
   calculateAvailableCels() {
     const upCell = upMove(this.getCurrentPosition(), this.getColor());
     this.availableCellsToMove.push(upCell);
+
+    const color = this.getColor();
+    const position = this.getCurrentPosition();
+    const coordinates = getCoordinatesByPosition(position);
+    const verticalShift = color === 'w' ? 1 : -1;
+    const leftPositionCoordinates = new Position(coordinates).verticalShift(verticalShift).horizontalShift(-1).getPostition();
+    const rightPositionCoordinates = new Position(coordinates).verticalShift(verticalShift).horizontalShift(1).getPostition();
+    
+    if (leftPositionCoordinates) {
+      const leftPosition = getPositionByCoordinates(leftPositionCoordinates);
+      const leftPiece = this.pieces.getPieceByPosition(leftPosition);
+
+      if (leftPiece && leftPiece.getColor() !== color) {
+        this.availableCellsToMove.push(leftPiece.getCurrentPosition());
+      } 
+    }
+
+    if (rightPositionCoordinates) {
+      const rightPosition = getPositionByCoordinates(rightPositionCoordinates);
+      const rightPiece = this.pieces.getPieceByPosition(rightPosition);
+
+      if (rightPiece && rightPiece.getColor() !== color) {
+        this.availableCellsToMove.push(rightPiece.getCurrentPosition());
+      } 
+    }
 
     if (!this.isMoved) {
       const cell = upMove(this.getCurrentPosition(), this.getColor(), 2);
