8e984b36924392e8241f977c4164cb733e1c20ee
yankuchinsky <89603879035@yandex.ru>
2022-07-15 04:00:18 +0200

    add king castle move

--- src/pieces/King.ts
+++ src/pieces/King.ts
@@ -1,9 +1,33 @@
 import Piece from './Piece';
 import { Position, getPositionByCoordinates, getCoordinatesByPosition } from '../helpers';
-
+import { globalGameState } from '../index';
 class King extends Piece {
   private hasCastled = false;
   private isKingMoved = false;
+
+  move(cell: { cellToMoveId: number, cell: HTMLDivElement }, callback?: Function) {
+    super.move(cell, callback);
+
+    const { cellToMoveId } = cell;
+    const startingPosition = this.getId();
+    const color = this.getColor();
+    
+    if (startingPosition + 2 === cellToMoveId) {
+      const board = globalGameState.getBoard();
+      const shift = color === 'w' ? 3 : 4;
+      const rook = this.pieces.getPieceById(startingPosition + shift)
+      const cellRef = board.getCellById(startingPosition + 1)!.cellRef;
+      rook?.move({ cellToMoveId: startingPosition + 1, cell: cellRef })
+    }
+
+    if (startingPosition - 2 === cellToMoveId) {
+      const board = globalGameState.getBoard();
+      const shift = color === 'w' ? 4 : 3;
+      const rook = this.pieces.getPieceById(startingPosition - shift)
+      const cellRef = board.getCellById(startingPosition - 1)!.cellRef;
+      rook?.move({ cellToMoveId: startingPosition - 1, cell: cellRef })
+    }
+  }
 
   calculateAvailableCels() {
     const curr = this.getCurrentPosition();
@@ -38,9 +62,10 @@ class King extends Piece {
   castleShort() {
     const position = this.getCurrentPosition();
     const coordinates = getCoordinatesByPosition(position);
-    const firstPieceCoordinates = new Position(coordinates).horizontalShift(1).getPostition()!;
-    const secondPieceCoordinates = new Position(coordinates).horizontalShift(2).getPostition()!;
-
+    const color = this.getColor();
+    const d = color === 'w' ? 1 : -1;
+    const firstPieceCoordinates = new Position(coordinates).horizontalShift(1 * d).getPostition()!;
+    const secondPieceCoordinates = new Position(coordinates).horizontalShift(2 * d).getPostition()!;
     const firstPiecePosition = getPositionByCoordinates(firstPieceCoordinates);
     const secondPiecePosition = getPositionByCoordinates(secondPieceCoordinates);
     const firstPiece = this.pieces.getPieceByPosition(firstPiecePosition);
@@ -56,9 +81,11 @@ class King extends Piece {
   castleLong() {
     const position = this.getCurrentPosition();
     const coordinates = getCoordinatesByPosition(position);
-    const firstPieceCoordinates = new Position(coordinates).horizontalShift(-1).getPostition()!;
-    const secondPieceCoordinates = new Position(coordinates).horizontalShift(-2).getPostition()!;
-    const thirdPieceCoordinates = new Position(coordinates).horizontalShift(-3).getPostition()!;
+    const color = this.getColor();
+    const d = color === 'w' ? -1 : 1;
+    const firstPieceCoordinates = new Position(coordinates).horizontalShift(d * 1).getPostition()!;
+    const secondPieceCoordinates = new Position(coordinates).horizontalShift(d * 2).getPostition()!;
+    const thirdPieceCoordinates = new Position(coordinates).horizontalShift(d * 3).getPostition()!;
 
 
     const firstPiecePosition = getPositionByCoordinates(firstPieceCoordinates);
